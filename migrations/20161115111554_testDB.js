6eaf5fed2c7dd730fb4e16a677ae7a9c94e002ac
lizzielashark <lizzie.s.sullivan@gmail.com>
2016-11-16 18:47:31 +0700

    testdb all up and running and seeds added

---
+++ migrations/20161115111554_testDB.js
@@ -0,0 +1,17 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTableIfNotExists('testDB', function(table) {
+
+    table.integer('id')
+    table.string('first_name')
+    table.string('last_name')
+    table.string('email')
+    table.integer('phone')
+    table.string('employer')
+    table.string('facebook_username')
+
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('testDB')
+};
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express')
 var app = express()
+var dotenv = require ('dotenv')
 var pg = require('pg')
-var knex = require('knex')
+var knex = require('knex')({client: 'pg'})
 
+connectionString = process.env.DATABASE_URL || 'postgres://localhost:5000/testDB';
 
 // var knexConfig = require('./knexfile')
 // var env = process.env.NODE_ENV || 'development'
@@ -11,3 +13,11 @@ var knex = require('knex')
 // if (env == 'development') {
 //   var dotenv = require('dotenv')
 // dotenv.load()
+
+
+
+app.get('/', function (req, res) {
+  res.send('Hello!')
+})
+
+app.listen(5000)
