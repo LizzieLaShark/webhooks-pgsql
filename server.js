0e40db7326d5f223ba88a1111b511a9178dcdfda
lizzielashark <lizzie.s.sullivan@gmail.com>
2016-12-01 13:45:55 +0700

    tweaking things to fix heroku H10 bug

--- server.js
+++ server.js
@@ -4,37 +4,15 @@ var dotenv = require ('dotenv')
 var pg = require('pg')
 var knex = require('knex')({client: 'pg'})
 
+var port = process.env.PORT || 5000
 
-// pg.defaults.ssl = true;
-// pg.connect(process.env.DATABASE_URL, function(err, client) {
-//   if (err) throw err;
-//   console.log('Connected to postgres! Getting schemas...');
-//
-//   client
-//     .query('SELECT testDB FROM testdbwebhooks.tables;')
-//     .on('row', function(row) {
-//       console.log(JSON.stringify(row));
-//     });
-// });
-
-
-
-//var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5000/testDB';
-
-// var knexConfig = require('./knexfile')
-// var env = process.env.NODE_ENV || 'development'
-// var knex = Knex(knexConfig[env])
-//
-// if (env == 'development') {
-//   var dotenv = require('dotenv')
-// dotenv.load()
 
 
 // DATABASE_URL = postgresql-graceful-53310
 
 
 app.get('/', function (req, res) {
-  res.send('Hello!')
+  res.send('BKK G lyf!')
 })
 
-app.listen(5000)
+app.listen(port)
