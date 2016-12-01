1a0cfbb6c2e66fd86c3618bf9c24e1e60018292e
lizzielashark <lizzie.s.sullivan@gmail.com>
2016-12-01 18:20:14 +0700

    testing endpoint url recieves payload properly

--- server.js
+++ server.js
@@ -7,8 +7,43 @@ var knex = require('knex')({client: 'pg'})
 var port = process.env.PORT || 5000
 
 
+app.post("/event", function(req, res) {
+  var events = req.body.events;
 
-// DATABASE_URL = postgresql-graceful-53310
+  console.log(events);
+
+  // Respond with a success code
+  res.send(200);
+});
+
+
+
+
+
+
+//var app = require('http').createServer(handler);
+// var statusCode = 200;
+//
+// function handler (req, res) {
+//   var data = '';
+//
+//   if (req.method == "POST") {
+//     req.on('data', function(chunk) {
+//       data += chunk;
+//     });
+//
+//     req.on('end', function() {
+//       console.log('Received body data:');
+//       console.log(data.toString());
+//     });
+//   }
+//
+//   res.writeHead(statusCode, {'Content-Type': 'text/plain'});
+//   res.end();
+// }
+// console.log("Returning status code " + statusCode.toString());
+
+
 
 
 app.get('/', function (req, res) {
