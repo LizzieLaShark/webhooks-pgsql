0cd2ff8bf56bd188de74bd39bd58414da3301e7e
lizzielashark <lizzie.s.sullivan@gmail.com>
2017-03-03 14:19:20 +1300

    tweaks

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
+const knex = require('knex')('development')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
