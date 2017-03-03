54abef2b223d25f0472cd41381a764333e770baf
lizzielashark <lizzie.s.sullivan@gmail.com>
2017-03-03 13:49:28 +1300

    db tweaks

--- server.js
+++ server.js
@@ -14,6 +14,22 @@ global.knex = knexGenerator(knexDbConfig)
 var port = process.env.PORT || 8080
 
 
+app.post("/createPerson", function(req, res) {
+
+  payload = req.body.payload.person
+  console.log("payload", payload)
+
+  knex('contacts').insert({contact_id: payload.id, contact_name: payload.first_name, email: payload.email, created_at: payload.created_at})
+  .then(function(data, err){
+    if(err){
+      console.log('error message: ', err)
+    } else {
+    console.log('check data is entered')
+    }
+  })
+})
+
+
 app.post("/test", function(req, res) {
 
   payload = req.body.payload.person
