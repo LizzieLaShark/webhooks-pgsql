16a90f20d9df8e187ab5f28483232f8234e65942
lizzielashark <lizzie.s.sullivan@gmail.com>
2017-03-06 15:47:38 +1300

    reconfigured the connection, used db url in server.js file

--- server.js
+++ server.js
@@ -4,15 +4,34 @@ var bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+//
+// var env = process.env.NODE_ENV || 'development'
+// var knexConfig = require('./knexfile.js')
+// var knexGenerator = require('knex')
+// var knexDbConfig = knexConfig[env]
+// global.knex = knexGenerator(knexDbConfig)
 
-var env = process.env.NODE_ENV || 'development'
-var knexConfig = require('./knexfile.js')
-var knexGenerator = require('knex')
-var knexDbConfig = knexConfig[env]
-global.knex = knexGenerator(knexDbConfig)
+
+var pg = require('pg');
+
+pg.defaults.ssl = true;
+
+pg.connect('postgres://dgxssvqjvwkrto:xDEl398dlARfdVAkOegZcwI1mD@ec2-54-243-207-190.compute-1.amazonaws.com:5432/d7vjen7acmospg', function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
+
+
+
+
 
 var port = process.env.PORT || 8080
-
 
 app.post("/test", function(req, res) {
 
